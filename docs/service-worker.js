34d51d36ef58b6a703ac8c91bd07e3a387f2cc32
slavagostev2-dot <slavagostev2@gmail.com>
2026-07-14 01:04:20 +0700

    Update Mini App offline cache v3

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,19 +1,38 @@
-const CACHE='betboom-monitor-v2';
-const SHELL=['./','./index.html','./styles.css?v=2.0.0','./core.js?v=2.0.0','./data.js?v=2.0.0','./views-main.js?v=2.0.0','./views-secondary.js?v=2.0.0','./interactions.js?v=2.0.0','./bootstrap.js?v=2.0.0','./manifest.webmanifest','./icon.svg'];
+'use strict';
+
+const CACHE='betboom-monitor-v3';
+const SHELL=[
+  './',
+  './index.html',
+  './styles.css?v=3.0.0',
+  './app.js?v=3.0.0',
+  './runtime-config.js?v=3.0.0',
+  './manifest.webmanifest',
+  './icon.svg',
+  './splash.svg'
+];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));
 });
 
 self.addEventListener('activate',event=>{
-  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
+      .then(()=>self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch',event=>{
-  if(event.request.method!=='GET'||new URL(event.request.url).origin!==location.origin)return;
-  event.respondWith(fetch(event.request).then(response=>{
-    const copy=response.clone();
-    caches.open(CACHE).then(cache=>cache.put(event.request,copy));
-    return response;
-  }).catch(()=>caches.match(event.request).then(response=>response||caches.match('./index.html'))));
+  if(event.request.method!=='GET')return;
+  const url=new URL(event.request.url);
+  if(url.origin!==location.origin)return;
+  event.respondWith(
+    fetch(event.request).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+      return response;
+    }).catch(()=>caches.match(event.request).then(response=>response||caches.match('./index.html')))
+  );
 });
