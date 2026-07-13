745c407c5cf7e650f32def09ffc3c2026432e3f5
slavagostev2-dot <slavagostev2@gmail.com>
2026-07-14 01:51:45 +0700

    Refresh BB V.G. Mini App offline cache

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const CACHE='betboom-monitor-v3';
+const CACHE='bb-vg-v4';
 const SHELL=[
   './',
   './index.html',
-  './styles.css?v=3.0.0',
-  './app.js?v=3.0.0',
-  './runtime-config.js?v=3.0.0',
+  './styles.css?v=4.0.0',
+  './app.js?v=4.0.0',
+  './runtime-config.js?v=4.0.0',
   './manifest.webmanifest',
   './icon.svg',
   './splash.svg'
@@ -15,24 +15,16 @@ const SHELL=[
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));
 });
-
 self.addEventListener('activate',event=>{
-  event.waitUntil(
-    caches.keys()
-      .then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
-      .then(()=>self.clients.claim())
-  );
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));
 });
-
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET')return;
   const url=new URL(event.request.url);
   if(url.origin!==location.origin)return;
-  event.respondWith(
-    fetch(event.request).then(response=>{
-      const copy=response.clone();
-      caches.open(CACHE).then(cache=>cache.put(event.request,copy));
-      return response;
-    }).catch(()=>caches.match(event.request).then(response=>response||caches.match('./index.html')))
-  );
+  event.respondWith(fetch(event.request).then(response=>{
+    const copy=response.clone();
+    caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+    return response;
+  }).catch(()=>caches.match(event.request).then(response=>response||caches.match('./index.html'))));
 });
