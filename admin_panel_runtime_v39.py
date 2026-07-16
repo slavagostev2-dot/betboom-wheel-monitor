b78b924ab42d1de419c44958e44d6136b67d979f
slavagostev2-dot <slavagostev2@gmail.com>
2026-07-16 15:39:07 +0700

    Remove obsolete v39 runtime alias

--- admin_panel_runtime_v39.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-from bbvg.bot.runtime import TelegramPanelRuntime as TelegramPanelRuntimeV39
-from bbvg.bot.runtime import main, self_test
-
-
-if __name__ == "__main__":
-    raise SystemExit(main())
